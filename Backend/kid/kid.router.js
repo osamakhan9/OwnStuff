ebd65cd3249efe3aef387405158fd2aa7bb786f5
mohammad osama <okosama06@gmail.com>
2023-04-05 20:02:00 +0530

    backend-men-women-electronic-kid-work-complete

---
+++ Backend/kid/kid.router.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express.Router()
+const Kid = require("./kid.module")
+
+app.get("/", async(req,res)=>{
+	try{
+		let data = await Kid.find()
+		res.send(data)
+
+	} catch(e){
+		res.send(e.massage)
+	}
+})
+
+app.post('/',async(req,res)=>{
+	// let data = req.body.area.split(",")
+	// res.send("osama")
+  const {name,quantity,image,hoverImage,price,offerprice,fimage,simage,timage}=req.body
+	try{
+		let kid = await Kid.create({
+			// id: data[0],
+			name,
+			quantity,
+			image,
+            hoverImage,
+			price,
+			offerprice,
+			fimage,
+			simage,
+			timage,
+		})
+		res.status(200).send('Added Successfully')
+	}catch(e){
+      // res.status(404).send(e.massage)
+	}
+})
+
+
+app.delete("/", async (req, res) => {
+	try {
+	  let exists = await Kid.findOneAndDelete({
+		id: req.body.id,
+		name: req.body.name,
+	  });
+	  console.log(exists, req.body.id, req.body.name);
+  
+	  res.status(200).send("Product deleted successfully");
+	} catch (e) {    
+	  res.send(e.massage);
+	}
+  });
+
+  module.exports = app
---
+++ Backend/men/men.router.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app = express.Router()
+const Men = require("./men.model")
+
+app.get("/", async(req,res)=>{
+	try{
+		let data = await Men.find()
+		res.send(data)
+
+	} catch(e){
+		res.send(e.massage)
+	}
+})
+
+app.post('/',async(req,res)=>{
+	// let data = req.body.area.split(",")
+	// res.send("osama")
+  const {name,quantity,image,hoverImage,price,offerprice,fimage,simage,timage}=req.body
+	try{
+		let men = await Men.create({
+			// id: data[0],
+			name,
+			quantity,
+			image,
+            hoverImage,
+			price,
+			offerprice,
+			fimage,
+			simage,
+			timage,
+		})
+		res.status(200).send('Added Successfully')
+	}catch(e){
+      // res.status(404).send(e.massage)
+	}
+})
+
+
+app.delete("/", async (req, res) => {
+	try {
+	  let exists = await Men.findOneAndDelete({
+		id: req.body.id,
+		name: req.body.name,
+	  });
+	  console.log(exists, req.body.id, req.body.name);
+  
+	  res.status(200).send("Product deleted successfully");
+	} catch (e) {    
+	  res.send(e.massage);
+	}
+  });
+
+  module.exports = app
